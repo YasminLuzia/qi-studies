c6a640a06eb3af4a920a579697e43d957066b31d
YasminL <yasmin.s.luzia@gmail.com>
2022-07-28 19:23:04 -0300

    Unziping old projects

---
+++ web1/TodasAulas/Lesson12JavaScript/js/validation.js
@@ -0,0 +1,12 @@
+function avisar() {
+    window.alert("Boa tarde!");
+}
+
+function abrirSite() {
+    window.open("https://qi.edu.br");
+}
+
+function mostrarDiaDaSemana() {
+    window.alert("Terca-feira.")
+}
+
